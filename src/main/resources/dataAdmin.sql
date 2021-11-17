795b177aecbd52fdfda8b94f0362d1688517b10a
Mario Parrilla Maroto <marioparrilla2@gmail.com>
2021-11-17 15:53:24 +0100

    data.sql add

---
+++ src/main/resources/dataAdmin.sql
@@ -0,0 +1,1 @@
+INSERT INTO `usuario` (`cms_admin`,`email`,`enabled`,`password`,`phonenumber`,`username`) VALUES ('1','admin@myeventsapp.com','1','$2a$10$cP3ScstTKcoRUuwXy3zeZOt4nprbSpb.V5DoS41qk9Jg08RWyvXNS','685456987','admin');
