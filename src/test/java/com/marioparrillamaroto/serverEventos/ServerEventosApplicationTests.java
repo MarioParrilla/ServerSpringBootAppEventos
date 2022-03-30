075a08793cdbfb7be4a2194c65ea02dfb526e816
Mario Parrilla Maroto <marioparrilla2@gmail.com>
2022-03-30 11:14:30 +0200

    GH

--- src/test/java/com/marioparrillamaroto/serverEventos/ServerEventosApplicationTests.java
+++ src/test/java/com/marioparrillamaroto/serverEventos/ServerEventosApplicationTests.java
@@ -2,17 +2,30 @@ package com.marioparrillamaroto.serverEventos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.marioparrillamaroto.serverEventos.controladores.ControladorUsuario;
+import com.marioparrillamaroto.serverEventos.controladores.controladorIndex;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
 
 @SpringBootTest
 class ServerEventosApplicationTests {
 
 	@Test
-	void prueba() {
+	void pruebaRespuestaLogin() {
+		controladorIndex controladorIndex = new controladorIndex();
+		String result = controladorIndex.login();
+		assertEquals(result, "login");
+	}
 
-		assertEquals(expected, actual);
+	@Test
+	void pruebaAgregarUsuario() {
+		ControladorUsuario cUsuario = new ControladorUsuario();
 
+		Model model = ;
+		String result = cUsuario.agregarUsuarioRoot(model, "prueba", "prueba", "prueba@prueba.com", "678434567");
+		assertEquals(result, "usuarios");
 	}
 
 }
